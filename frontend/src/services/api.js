a9d973b0ba664786f8ed4491585ae75d1bfb8c32
daksh75 <devjakhrr7@email.com>
2026-07-21 14:03:45 +0530

    capstone-assignment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://capstone-project-backend-tvmj.onrender.com/api",
 });
 
 export default API;
